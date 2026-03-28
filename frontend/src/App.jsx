a44c210a35223d02a75859306683f5df20f5822f
OhaseeLanka <ohasilanka6@gmail.com>
2026-03-28 10:45:29 +0530

    Updated app routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,17 @@ import Dashboard from "./pages/Dashboard";
 import Admin from "./pages/Admin";
 import NotificationProvider from "./context/NotificationContext";
 import BookingProvider from "./context/BookingContext";
+import LaundryPage from "./pages/LaundryPage";
 
 function App() {
   return (
     <NotificationProvider>
       <BookingProvider>
         <Router>
-          <div className="bg-gray-50 min-h-screen dark:bg-gray-900 transition-colors">
-            <Header />
+          <div className="bg-gray-50 min-h-screen transition-colors">
             <Routes>
               <Route path='/' element={<HomePage />} />
-              <Route path="/new" element={<Home />} />
+              <Route path="/laundry" element={<LaundryPage />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/admin" element={<Admin />} />
             </Routes>
