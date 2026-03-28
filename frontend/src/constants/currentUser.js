18368d99f9f2b1fb190f5434c6836b8a56e5365f
OhaseeLanka <ohasilanka6@gmail.com>
2026-03-28 10:47:32 +0530

    Fixed current user store

---
+++ frontend/src/constants/currentUser.js
@@ -0,0 +1,9 @@
+export const CURRENT_USER = {
+  id: '69c2654f7e2400e0cf1cc40d',
+  name: 'Ohasee',
+  email: 'ohasee@email.com',
+  role: 'student',
+  roomNumber: 'Room 101',
+  hostelBlock: 'Block A',
+  isActive: true,
+};
